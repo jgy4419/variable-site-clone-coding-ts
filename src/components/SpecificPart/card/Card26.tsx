b4e8b8951d49d7bcc4ecea72c576ddbe6da91ba6
정규영 <jgy_98@naver.com>
2024-01-27 12:53:05 +0900

    design : 2024-01-27 Card26 회전 시 자연스럽게 회전하도록 수정

--- src/components/SpecificPart/card/Card26.tsx
+++ src/components/SpecificPart/card/Card26.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import * as S from "./style/Card26";
 import appleCardImg from "../../../assets/images/appleCard.png";
+import iphoneImg from "../../../assets/images/iphoneImg.png";
+import iphoneImgFront from "../../../assets/images/iphoneImgFront.png";
 
 const Card26 = () => {
     const [frontContent, setFrontContent] = useState({
-        phoneImg: "https://www.ktmmobile.com/resources/upload/phone/RGLm6vp1dieBCc2.png",
+        phoneImg: iphoneImgFront,
         cardImg: appleCardImg,
         title: "Created By Apple. Powered by iPhone.",
     });
@@ -18,29 +20,33 @@ const Card26 = () => {
            You can sign up in as little
            as a minute and start using it right away with Apple Pay.
         `,
-        phoneImg: "https://www.ktmmobile.com/resources/upload/phone/RGLm6vp1dieBCc2.png"
+        phoneImg: iphoneImg
     });
-    const [contentState, setContentState] = useState(true);
+    const [contentState, setContentState] = useState(false);
 
     return (
         <S.Container>
             <S.Inner contentState={contentState}>
                 {
-                    contentState === false ? <S.FrontContainer>
-                        <S.FrontContainerLeftImg src={frontContent.phoneImg}/>
-                        <S.FrontContainerRight>
-                            <S.FrontTitle>{frontContent.title}</S.FrontTitle>
-                            <S.FrontContainerCardImg src={frontContent.cardImg}/>
-                            <S.ContentStateButton contentState={contentState} onClick={() => setContentState(prev => !prev)}>+</S.ContentStateButton>
-                        </S.FrontContainerRight>
+                    contentState === false ? <S.FrontContainer contentState={contentState}  >
+                        <S.FrontInner>
+                            <S.FrontContainerLeftImg src={frontContent.phoneImg}/>
+                            <S.FrontContainerRight>
+                                <S.FrontTitle>{frontContent.title}</S.FrontTitle>
+                                <S.FrontContainerCardImg src={frontContent.cardImg}/>
+                                <S.ContentStateButton contentState={contentState} onClick={() => setContentState(prev => !prev)}>+</S.ContentStateButton>
+                            </S.FrontContainerRight>
+                        </S.FrontInner>
                     </S.FrontContainer>
-                    : <S.BackContainer>
-                        <S.BackContainerLeft>
-                            <S.BackTitle>{backContent.title}</S.BackTitle>
-                            <S.BackDescription>{backContent.description}</S.BackDescription>
-                            <S.ContentStateButton contentState={contentState} onClick={() => setContentState(prev => !prev)}>+</S.ContentStateButton>
-                        </S.BackContainerLeft>
-                        <S.BackRightImg src={backContent.phoneImg}/>
+                    : <S.BackContainer contentState={contentState}>
+                        <S.BackInner>
+                            <S.BackContainerLeft>
+                                <S.BackTitle>{backContent.title}</S.BackTitle>
+                                <S.BackDescription>{backContent.description}</S.BackDescription>
+                                <S.ContentStateButton contentState={contentState} onClick={() => setContentState(prev => !prev)}>+</S.ContentStateButton>
+                            </S.BackContainerLeft>
+                            <S.BackRightImg src={backContent.phoneImg}/>
+                        </S.BackInner>
                     </S.BackContainer>
                 }
             </S.Inner>
--- src/components/SpecificPart/card/style/Card26.ts
+++ src/components/SpecificPart/card/style/Card26.ts
@@ -1,4 +1,14 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const appertAnimation = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
+
 
 export const Container = styled.div`
     position: relative;
@@ -16,11 +26,12 @@ export const Inner = styled.div<{contentState: boolean}>`
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: .3s;
+    transition: 1s;
+    transform-style: preserve-3d;
     transform: ${props => props.contentState === true ? "rotateX(180deg)" : "rotateX(0deg)"};
 `;
 
-export const FrontContainer = styled.div`
+export const FrontContainer = styled.div<{contentState: boolean}>`
     width: 100%;
     height: 80%;
     display: flex;
@@ -29,35 +40,54 @@ export const FrontContainer = styled.div`
     background-color: #fff;
     border-radius: 20px;
     overflow: hidden;
+    opacity: ${props => props.contentState === true ? 0 : 1};
+`;
+
+export const FrontInner = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    animation: ${appertAnimation} 1.5s;
 `;
 
 export const FrontContainerLeftImg = styled.img`
     margin-top: 30%;
+    width: 800px;
 `;
 
 export const FrontContainerRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
 `;
 
 export const FrontTitle = styled.h1`
     margin-left: 10%;
+    margin-top: 10%;
     width: 60%;
     font-size: 40px;
 `;
 
 export const FrontContainerCardImg = styled.img`
     margin-left: -10%;
-
 `;
 
-export const BackContainer = styled.div`
+export const BackContainer = styled.div<{contentState: boolean}>`
     width: 100%;
     height: 80%;
     background-color: #fff;
     border-radius: 20px;
+`;
+
+export const BackInner = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    // transition: 1s;
+    width: 100%;
+    height: 100%;
     transform: rotateX(180deg);
+    animation: ${appertAnimation} 1.5s;
 `;
 
 export const BackContainerLeft = styled.div`
