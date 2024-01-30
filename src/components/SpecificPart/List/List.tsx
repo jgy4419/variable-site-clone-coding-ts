9ba454195002eb65de4f599f9d849a2683988afa
정규영 <jgy_98@naver.com>
2024-01-30 21:28:37 +0900

    feat : 2024.01.30 List1 (Draggable List)

---
+++ src/components/SpecificPart/List/List.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import List1 from "./List1";
+
+const List = () => {
+    return (
+        <div>
+            <List1/>
+        </div>
+    );
+};
+
+export default List;
